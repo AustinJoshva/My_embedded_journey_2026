15b69e54a3b7e62c16a57ffc8647341cd1118858
Austin Joshva <austinjoshva607@gmail.com>
2026-08-18 18:31:18 +0530

    Add Button_Controlled_LED for Arduino

---
+++ Arduino/Button_Controlled_LED/Button_Controlled_LED.ino
@@ -0,0 +1,18 @@
+int buttonPin = 2;
+int ledPin = 3;
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+}
